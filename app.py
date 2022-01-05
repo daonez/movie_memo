ff9a5fa6bb7c03202009411e952df795e62cc9a0
daonez <cyongchoi@gmail.com>
2022-01-05 21:36:00 +0900

    duplicated line delete

--- app.py
+++ app.py
@@ -67,10 +67,3 @@ def delete_article():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-from flask import Flask
-app = Flask(__name__)
-
-
-
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
